9b1ebd43b68de8c9c1ede4aec16dfc59096faf7c
Nordine Bittich <contact@bittich.be>
2025-02-03 12:14:06 +0100

    disable async

--- src/main/java/lblod/info/datasetdiff/AppService.java
+++ src/main/java/lblod/info/datasetdiff/AppService.java
@@ -7,7 +7,6 @@ import mu.semte.ch.lib.dto.DataContainer;
 import mu.semte.ch.lib.utils.ModelUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -33,96 +32,98 @@ public class AppService {
         this.taskService = taskService;
     }
 
-    @Async
     public void runAsync(String deltaEntry) {
 
-        if (!taskService.isTask(deltaEntry))
-            return;
-        var task = taskService.loadTask(deltaEntry);
+        Thread.startVirtualThread(() -> {
 
-        if (task == null || StringUtils.isEmpty(task.getOperation())) {
-            log.debug("task or operation is empty for delta entry {}", deltaEntry);
-            return;
-        }
+            if (!taskService.isTask(deltaEntry))
+                return;
+            var task = taskService.loadTask(deltaEntry);
 
-        if (Constants.TASK_HARVESTING_DATASET_DIFF.equals(task.getOperation())) {
-            try {
-
-                taskService.updateTaskStatus(task, Constants.STATUS_BUSY);
-                var inputContainer = taskService.selectInputContainer(task).get(0);
-                log.info("input container: {}", inputContainer);
-                var countTriples = taskService.countTriplesFromFileInputContainer(
-                        inputContainer.getGraphUri());
-                var pagesCount = countTriples > defaultLimitSize
-                        ? countTriples / defaultLimitSize
-                        : 1;
-
-                var fileContainer = DataContainer.builder().build();
-
-                var graphContainer = DataContainer.builder().build();
-                var resultContainer = DataContainer.builder().graphUri(graphContainer.getUri()).build();
-
-                for (var i = 0; i <= pagesCount; i++) {
-                    var threads = new ArrayList<Thread>();
-                    var offset = i * defaultLimitSize;
-                    var importedTriples = taskService.fetchTripleFromFileInputContainer(
-                            inputContainer.getGraphUri(), defaultLimitSize, offset);
-                    for (var mdb : importedTriples) {
-                        threads.add(Thread.startVirtualThread(() -> {
-                            var previousCompletedModel = taskService.fetchTripleFromPreviousJobs(task,
-                                    mdb.derivedFrom());
-                            sleep(sleepMs);
-                            var newInserts = ModelUtils.difference(mdb.model(), previousCompletedModel);
-                            var toRemoveOld = ModelUtils.difference(previousCompletedModel, mdb.model());
-                            var intersection = ModelUtils.intersection(mdb.model(), previousCompletedModel);
-                            if (!newInserts.isEmpty()) {
-                                var dataDiffContainer = fileContainer.toBuilder()
-                                        .graphUri(taskService.writeTtlFile(
-                                                task.getGraph(), newInserts,
-                                                "new-insert-triples.ttl", mdb.derivedFrom()))
-                                        .build();
-                                taskService.appendTaskResultFile(task, dataDiffContainer);
-
-                                taskService.appendTaskResultFile(
-                                        task, graphContainer.toBuilder()
-                                                .graphUri(dataDiffContainer.getGraphUri())
-                                                .build());
-                            }
-                            if (!toRemoveOld.isEmpty()) {
-                                var dataRemovalsContainer = fileContainer.toBuilder()
-                                        .graphUri(taskService.writeTtlFile(
-                                                task.getGraph(), toRemoveOld,
-                                                "to-remove-triples.ttl", mdb.derivedFrom()))
-                                        .build();
-                                taskService.appendTaskResultFile(task, dataRemovalsContainer);
-                            }
-                            if (!intersection.isEmpty()) {
-                                var dataIntersectContainer = fileContainer.toBuilder()
-                                        .graphUri(taskService.writeTtlFile(
-                                                task.getGraph(), intersection,
-                                                "intersect-triples.ttl", mdb.derivedFrom()))
-                                        .build();
-                                taskService.appendTaskResultFile(task, dataIntersectContainer);
-                            }
-                        }));
-                        sleep(sleepMs);
-                    }
-                    for (var thread : threads) {
-                        thread.join();
-                    }
-                }
-
-                taskService.appendTaskResultGraph(task, resultContainer);
-                taskService.updateTaskStatus(task, Constants.STATUS_SUCCESS);
-                log.info("Done with success for task {}", task.getId());
-            } catch (Throwable e) {
-                log.error("Error:", e);
-                taskService.updateTaskStatus(task, Constants.STATUS_FAILED);
-                taskService.appendTaskError(task, e.getMessage());
+            if (task == null || StringUtils.isEmpty(task.getOperation())) {
+                log.debug("task or operation is empty for delta entry {}", deltaEntry);
+                return;
             }
-        } else {
-            log.debug("unknown operation '{}' for delta entry {}",
-                    task.getOperation(), deltaEntry);
-        }
+
+            if (Constants.TASK_HARVESTING_DATASET_DIFF.equals(task.getOperation())) {
+                try {
+
+                    taskService.updateTaskStatus(task, Constants.STATUS_BUSY);
+                    var inputContainer = taskService.selectInputContainer(task).get(0);
+                    log.info("input container: {}", inputContainer);
+                    var countTriples = taskService.countTriplesFromFileInputContainer(
+                            inputContainer.getGraphUri());
+                    var pagesCount = countTriples > defaultLimitSize
+                            ? countTriples / defaultLimitSize
+                            : 1;
+
+                    var fileContainer = DataContainer.builder().build();
+
+                    var graphContainer = DataContainer.builder().build();
+                    var resultContainer = DataContainer.builder().graphUri(graphContainer.getUri()).build();
+
+                    for (var i = 0; i <= pagesCount; i++) {
+                        var threads = new ArrayList<Thread>();
+                        var offset = i * defaultLimitSize;
+                        var importedTriples = taskService.fetchTripleFromFileInputContainer(
+                                inputContainer.getGraphUri(), defaultLimitSize, offset);
+                        for (var mdb : importedTriples) {
+                            threads.add(Thread.startVirtualThread(() -> {
+                                var previousCompletedModel = taskService.fetchTripleFromPreviousJobs(task,
+                                        mdb.derivedFrom());
+                                sleep(sleepMs);
+                                var newInserts = ModelUtils.difference(mdb.model(), previousCompletedModel);
+                                var toRemoveOld = ModelUtils.difference(previousCompletedModel, mdb.model());
+                                var intersection = ModelUtils.intersection(mdb.model(), previousCompletedModel);
+                                if (!newInserts.isEmpty()) {
+                                    var dataDiffContainer = fileContainer.toBuilder()
+                                            .graphUri(taskService.writeTtlFile(
+                                                    task.getGraph(), newInserts,
+                                                    "new-insert-triples.ttl", mdb.derivedFrom()))
+                                            .build();
+                                    taskService.appendTaskResultFile(task, dataDiffContainer);
+
+                                    taskService.appendTaskResultFile(
+                                            task, graphContainer.toBuilder()
+                                                    .graphUri(dataDiffContainer.getGraphUri())
+                                                    .build());
+                                }
+                                if (!toRemoveOld.isEmpty()) {
+                                    var dataRemovalsContainer = fileContainer.toBuilder()
+                                            .graphUri(taskService.writeTtlFile(
+                                                    task.getGraph(), toRemoveOld,
+                                                    "to-remove-triples.ttl", mdb.derivedFrom()))
+                                            .build();
+                                    taskService.appendTaskResultFile(task, dataRemovalsContainer);
+                                }
+                                if (!intersection.isEmpty()) {
+                                    var dataIntersectContainer = fileContainer.toBuilder()
+                                            .graphUri(taskService.writeTtlFile(
+                                                    task.getGraph(), intersection,
+                                                    "intersect-triples.ttl", mdb.derivedFrom()))
+                                            .build();
+                                    taskService.appendTaskResultFile(task, dataIntersectContainer);
+                                }
+                            }));
+                            sleep(sleepMs);
+                        }
+                        for (var thread : threads) {
+                            thread.join();
+                        }
+                    }
+
+                    taskService.appendTaskResultGraph(task, resultContainer);
+                    taskService.updateTaskStatus(task, Constants.STATUS_SUCCESS);
+                    log.info("Done with success for task {}", task.getId());
+                } catch (Throwable e) {
+                    log.error("Error:", e);
+                    taskService.updateTaskStatus(task, Constants.STATUS_FAILED);
+                    taskService.appendTaskError(task, e.getMessage());
+                }
+            } else {
+                log.debug("unknown operation '{}' for delta entry {}",
+                        task.getOperation(), deltaEntry);
+            }
+        });
     }
 }
--- src/main/java/lblod/info/datasetdiff/DatasetDiffApplication.java
+++ src/main/java/lblod/info/datasetdiff/DatasetDiffApplication.java
@@ -1,17 +1,55 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
 package lblod.info.datasetdiff;
 
+import com.github.jsonldjava.shaded.com.google.common.collect.Maps;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import mu.semte.ch.lib.handler.DefaultExceptionHandler;
+import mu.semte.ch.lib.utils.SparqlClient;
+import mu.semte.ch.lib.utils.SparqlQueryStore;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.text.CaseUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-
-import mu.semte.ch.lib.config.CoreConfig;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
-@Import(CoreConfig.class)
+@Slf4j
+@Import({ SparqlClient.class, DefaultExceptionHandler.class })
 public class DatasetDiffApplication {
+    @Value("${sparql.queryStore.path:classpath*:sparql}/*.sparql")
+    private Resource[] queries;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DatasetDiffApplication.class, args);
-	}
+    @Bean
+    public SparqlQueryStore sparqlQueryLoader() {
+        log.info("Adding {} queries to the store", this.queries.length);
+        Map<String, String> queriesMap = (Map) Arrays.stream(this.queries).map((r) -> {
+            try {
+                String key = CaseUtils.toCamelCase(FilenameUtils.removeExtension(r.getFilename()), false,
+                        new char[] { '-' });
+                return Maps.immutableEntry(key, IOUtils.toString(r.getInputStream(), StandardCharsets.UTF_8));
+            } catch (IOException var2) {
+                throw new RuntimeException(var2);
+            }
+        }).peek((e) -> {
+            log.info("query {} added to the store", e.getKey());
+        }).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        return () -> {
+            return queriesMap;
+        };
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DatasetDiffApplication.class, args);
+    }
 }
